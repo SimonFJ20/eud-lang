cc16243671226cc756ea211d22889c4e07047222
camper0008 <spetakel123@gmail.com>
2022-04-04 12:55:24 +0200

    cli

--- cli/cli.go
+++ cli/cli.go
@@ -1,12 +1,81 @@
 package cli
 
-import "fmt"
+import (
+	"errors"
+	"eud/bytecode"
+	"eud/parser"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func fileExists(filename string) bool {
+	_, err := os.Stat(filename)
+	if errors.Is(err, os.ErrNotExist) {
+		return false
+	} else if err != nil {
+		panic("error checking file existance")
+	}
+	return true
+}
 
 func Cli() {
-	var input string
-	_, err := fmt.Scanf("%s", &input)
-	if err != nil {
-		panic("err")
+	files := os.Args[1:]
+
+	if len(files) == 0 {
+		fmt.Println("no files given")
+		os.Exit(1)
 	}
-	fmt.Printf("\"%s\" yourself, fuck you, not implemented\n", input)
+
+	for i := 0; i < len(files); i++ {
+		if !fileExists(files[i]) {
+			fmt.Printf("file %q does not exist\n", files[i])
+			os.Exit(1)
+		}
+	}
+
+	for i := 0; i < len(files); i++ {
+		file_bytes, err := ioutil.ReadFile(files[i])
+
+		if err != nil {
+			panic(err)
+		}
+
+		text := string(file_bytes)
+
+		fmt.Printf("\033[1;36mInput:\033[0m\n\"%s\"\n", text)
+
+		println("\033[1;36mTokenizing text:\033[0m")
+
+		tokens := parser.TokenizeString(text)
+
+		current_token := tokens
+		for current_token != nil {
+			println(current_token.Type.String())
+			current_token = current_token.Next
+		}
+
+		println("\033[1;36mParsing tokens:\033[0m")
+
+		p := parser.Parser{}
+
+		ast := p.Parse(tokens)
+
+		fmt.Printf("%s\n", ast)
+
+		println("\033[1;36mCompiling AST:\033[0m")
+
+		program, _ := bytecode.Compile(ast)
+
+		for i := range program.Instructions {
+			fmt.Println(program.Instructions[i].String())
+		}
+
+		println("\033[1;36mRunning bytecode:\033[0m")
+
+		program.RunWithDebug = true
+		runtime := bytecode.Run(program)
+
+		fmt.Printf("\033[1;36mResult:\033[0m\n%s = %d\n", text, runtime.Stack[0].(bytecode.I32Value).Value)
+	}
 }
--- main.go
+++ main.go
@@ -1,50 +1,7 @@
 package main
 
-import (
-	"eud/bytecode"
-	"eud/parser"
-	"fmt"
-)
+import "eud/cli"
 
 func main() {
-
-	text := "(3 + 4) * 5"
-
-	fmt.Printf("\033[1;36mInput:\033[0m\n\"%s\"\n", text)
-
-	println("\033[1;36mTokenizing text:\033[0m")
-
-	tokens := parser.TokenizeString(text)
-
-	current_token := tokens
-	for current_token != nil {
-		println(current_token.Type.String())
-		current_token = current_token.Next
-	}
-
-	println("\033[1;36mParsing tokens:\033[0m")
-
-	p := parser.Parser{}
-
-	ast := p.Parse(tokens)
-
-	fmt.Printf("%s\n", ast)
-
-	println("\033[1;36mCompiling AST:\033[0m")
-
-	program, _ := bytecode.Compile(ast)
-
-	for i := range program.Instructions {
-		fmt.Println(program.Instructions[i].String())
-	}
-
-	println("\033[1;36mRunning bytecode:\033[0m")
-
-	program.RunWithDebug = true
-	runtime := bytecode.Run(program)
-
-	fmt.Printf("\033[1;36mResult:\033[0m\n%s = %d\n", text, runtime.Stack[0].(bytecode.I32Value).Value)
-
-	// cli.Cli(
-	// panic("not implemented i guess")
+	cli.Cli()
 }
